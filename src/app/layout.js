31065dc681d85eafbf5efd8b86ac460cd39f7ad4
Abhishekmathur00 <abhishekmathur7575@gmail.com>
2025-01-29 11:55:46 +0530

    Add gtag link

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/common/header/Navbar";
 import Footer from "@/components/common/footer/Footer";
 import Whatsapp from "@/components/common/Whatsapp/Whatsapp";
-
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,6 +14,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js)  */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-4MZYLFVBN1"></Script>
+        <Script>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-4MZYLFVBN1'); `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <Navbar />
         {children}
