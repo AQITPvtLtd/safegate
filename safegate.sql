59a8f7400acab7a2efbf3740ce4de0a0eddc60d9
abhishek92132 <abhishek.singh92056@gmail.com>
2025-08-12 16:51:38 +0530

    change database

--- safegate.sql
+++ safegate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 12, 2025 at 12:46 PM
+-- Generation Time: Aug 12, 2025 at 01:20 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
